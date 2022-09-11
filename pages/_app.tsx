22fd692392feacbda694578087cb7927f021fed2
sjy <stseo012@gmail.com>
2022-09-11 13:03:29 +0900

    feat: app 컴포넌트

---
+++ pages/_app.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+
+const App = ({ Component, pageProps }: AppProps) => {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+
+export default App;
