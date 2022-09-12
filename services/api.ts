47639795a54f2e33797ebe29c32763240d2c5503
sjy <stseo012@gmail.com>
2022-09-12 23:44:47 +0900

    feat: 나의 포켓몬조회하고 추가하는 api 함수 추가

--- services/api.ts
+++ services/api.ts
@@ -1,11 +1,8 @@
 import axios from 'axios';
-
-export const api = axios.create({
-  baseURL: 'https://pokeapi.co/api/v2',
-});
-
-export const fetchPokemons = async ({ pageParam = 40 }) => {
-  const { data } = await api.get('/pokemon', {
+import { URL } from '../constants';
+import { IPokemon } from '../types/IPokemon';
+export const fetchPokemons = async ({ pageParam = 0 }) => {
+  const { data } = await axios.get(`${URL.POKEMON_URL}/pokemon`, {
     params: {
       limit: 40,
       offset: pageParam,
@@ -15,6 +12,25 @@ export const fetchPokemons = async ({ pageParam = 40 }) => {
 };
 
 export const fetchPokemonDetails = async (pokemonID: number) => {
-  const { data } = await api.get(`/pokemon/${pokemonID}`);
+  const { data } = await axios.get(`${URL.POKEMON_URL}/pokemon/${pokemonID}`);
+  return data;
+};
+
+export const fetchMyPokemons = async () => {
+  try {
+    const res = await axios.get(`${URL.SERVER_URL}/pokemons`);
+    return res.data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const fetchAddMyPokemon = async (
+  newPokemon: IPokemon
+): Promise<IPokemon> => {
+  const { data } = await axios.post<IPokemon>(
+    `${URL.SERVER_URL}/pokemons`,
+    newPokemon
+  );
   return data;
 };
