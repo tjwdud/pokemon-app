8175af13c13ecc3f690c6c93f3329502b1ee1d28
sjy <stseo012@gmail.com>
2022-09-12 23:50:21 +0900

    feat: 상수 값

--- constants/index.ts
+++ constants/index.ts
@@ -1,1 +1,10 @@
 export const OFFSET = 80;
+export const POKEMON_FIRST_ID = 1;
+export const POKEMON_LAST_ID = 905;
+export const RANDOM_CARD_NUMBER = 3;
+export const NUMBER_OF_GAME_POKEMON = 1;
+
+export const URL = {
+  SERVER_URL: 'http://localhost:8989',
+  POKEMON_URL: 'https://pokeapi.co/api/v2',
+};
