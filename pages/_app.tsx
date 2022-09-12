320fa9ce67ed5fa4ba6d4957c392faa62b268f9b
sjy <stseo012@gmail.com>
2022-09-12 23:51:51 +0900

    feat: 리액트 쿼리 옵션 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,26 @@
 import { useState } from 'react';
-import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
+const queryOption = {
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      staleTime: 1000 * 60 * 5,
+      cacheTime: 1000 * 60 * 5,
+    },
+  },
+};
+
 const App = ({ Component, pageProps }: AppProps) => {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(() => new QueryClient(queryOption));
 
   return (
     <QueryClientProvider client={queryClient}>
