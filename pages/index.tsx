977b6ccc493f02825a10b04c742c3797682003ee
tjwdud <>
2022-09-15 18:44:09 +0900

    feat: 공통 부분 레이아웃 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,8 @@
 import type { NextPage } from 'next';
-
 import PokemonList from '../components/PokemonList';
-import Image from 'next/image';
-import Link from 'next/Link';
 const Home: NextPage = () => {
   return (
     <div>
-      <div className="flex">
-        <Image src="/assets/img/logo.png" width={200} height={100} alt="logo" />
-        <Link href={'/game'}>
-          <a>게임하기</a>
-        </Link>
-      </div>
-
       <PokemonList />
     </div>
   );
