73fdf8cdbbb65b8667c7fb86fc6e15884fb70a73
tjwdud <>
2022-09-15 18:49:23 +0900

    feat: 게임 시작 기능 추가

--- pages/game/index.tsx
+++ pages/game/index.tsx
@@ -1,25 +1,26 @@
-import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import GetNewPokemonModal from './components/GetNewPokemonModal';
 import { useMyPokemon } from '../../hooks/useMyPokemon';
 import PokemonDetailCard from '@components/PokemonDetailCard';
 import { IPokemon } from 'types/IPokemon';
-import {
-  dehydrate,
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query';
+import { dehydrate, QueryClient } from '@tanstack/react-query';
 import { fetchMyPokemons } from '../../services/api';
-import { NUMBER_OF_GAME_POKEMON } from '../../constants';
-import { getRandomIds } from '../../utils/randomUtil';
-import { usePokemonDetails } from 'hooks/usePokemonDetails';
+import { isEmptyObj } from '../../utils/gameUtils';
+
 import GameBoard from './components/GameBoard';
+
 const Game = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const { data, isLoading, isFetching } = useMyPokemon();
-  const gamePokemonId = getRandomIds(NUMBER_OF_GAME_POKEMON);
+  const [myPokemon, setMyPokemon] = useState<IPokemon>({});
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [isStartGame, setIsStartGame] = useState(false);
 
-  const startGame = () => {};
+  const { data, isLoading, isFetching } = useMyPokemon();
+
+  const startGame = () => {
+    setIsStartGame(true);
+    setIsDisabled(false);
+  };
 
   useEffect(() => {
     if (data && data.length === 0) {
@@ -27,16 +28,45 @@ const Game = () => {
       return;
     }
   }, [data]);
-
+  const handlePickMyPokemon = (pokemon: IPokemon) => {
+    setMyPokemon(pokemon);
+    setIsDisabled(true);
+  };
   if (isLoading) return <div>로딩</div>;
+
   return (
     <div className="flex items-center">
-      <div className="mt-36 mr-auto ml-auto flex flex-col">
-        <GameBoard gamePokemonId={gamePokemonId} />
-        <div className=" w-[900px] min-h-[400px] bg-white mt-10 rounded-3xl">
-          <p className="text-gray-500 ml-10 text-3xl mt-5">나의 포켓몬</p>
-          {data.map((pokemon) => (
-            <PokemonDetailCard pokemonInfo={pokemon} key={pokemon.id} />
+      <div className="mt-36 mr-auto ml-auto flex">
+        {isStartGame ? (
+          <GameBoard myPokemon={myPokemon} />
+        ) : (
+          <div className="flex flex-col bg-bgImg w-[900px] h-[600px] bg-cover bg-backOpacity bg-no-repeat rounded-3xl rounded-r-3xl">
+            <button
+              onClick={() => startGame()}
+              className="mt-[30%] w-[200px] text-white bg-gray-800 rounded-full px-5 py-2.5  mb-2 m-auto"
+            >
+              게임시작
+            </button>
+          </div>
+        )}
+
+        <div className=" w-[900px] h-[600px] bg-white rounded-3xl overflow-y-auto ml-5 scrollbar-hide">
+          <p className="text-gray-500 ml-10 text-3xl mt-5 mb-5 ">나의 포켓몬</p>
+          {data.map((pokemon: IPokemon) => (
+            <div
+              onClick={() => handlePickMyPokemon(pokemon)}
+              key={pokemon.id}
+              className={`${
+                (isDisabled || !isStartGame) && 'pointer-events-none opacity-30'
+              }`}
+            >
+              <PokemonDetailCard
+                pokemonInfo={pokemon}
+                layoutDirection={'row'}
+                isBig={'false'}
+                key={pokemon.id}
+              />
+            </div>
           ))}
         </div>
       </div>
