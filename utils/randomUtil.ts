e7e4ba8c2414ef0ba3295809726f5363edbcbff0
tjwdud <>
2022-09-15 18:45:48 +0900

    feat: 유틸함수 타입추가

--- utils/randomUtil.ts
+++ utils/randomUtil.ts
@@ -1,14 +1,11 @@
 import { POKEMON_LAST_ID } from '../constants/index';
 
-export const getRandomIds = (requireRandomNumber: number) => {
-  const ids = new Set();
-  if (requireRandomNumber === 1) {
-    return Math.floor(Math.random() * POKEMON_LAST_ID + 1);
-  }
+export const getRandomIds = (requireRandomNumber: number): Array<number> => {
+  const ids: Set<number> = new Set();
+
   while (true) {
     if (ids.size === requireRandomNumber) break;
     ids.add(Math.floor(Math.random() * POKEMON_LAST_ID + 1));
   }
-
   return Array.from(ids);
 };
