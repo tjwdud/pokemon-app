d6e5ef139adcdb366047d30e4bd51431a18c1071
tjwdud <>
2022-09-15 18:43:03 +0900

    style: 포켓몬 상세 페이지 스타일 수정

--- components/PokemonDetailCard.tsx
+++ components/PokemonDetailCard.tsx
@@ -1,15 +1,57 @@
+import HpBar from 'pages/game/components/HpBar';
 import { IPokemon } from 'types/IPokemon';
 
-const PokemonDetailCard = ({ pokemonInfo }: { pokemonInfo: IPokemon }) => {
-  const { id, name, hp, attack, defense, image } = pokemonInfo;
-  console.log(image, 'image');
+const PokemonDetailCard = ({
+  pokemonInfo,
+  layoutDirection,
+  isBig,
+}: {
+  pokemonInfo: IPokemon;
+  layoutDirection: string;
+  isBig: string;
+}) => {
+  const { id, name, hp, attack, defense, image, speed } = pokemonInfo;
+  console.log(isBig);
   return (
-    <div>
-      <img src={image} alt={name} className="w-[200px] h-[200px]" />
-      <p className="text-center">{name}</p>
-      <p className="text-center">체력: {hp}</p>
-      <p className="text-center">공격력: {attack}</p>
-      <p className="text-center">방어력: {defense}</p>
+    <div
+      className={`flex ${
+        layoutDirection === 'col' ? 'flex-col' : 'flex-row ml-20'
+      }`}
+    >
+      <img
+        src={image}
+        alt={name}
+        className={`${
+          isBig === 'true' ? 'w-[600px] h-[600px]' : 'w-[200px] h-[200px]'
+        }  `}
+      />
+
+      <div
+        className={`${
+          layoutDirection === 'row' ? 'mt-10 ml-20 w-[50%]' : 'w-[100%]'
+        }`}
+      >
+        <div className={`${layoutDirection === 'row' ? 'flex' : 'flex-col '}`}>
+          {name}
+        </div>
+
+        <div className={`${layoutDirection === 'row' ? 'flex' : 'flex-col'}`}>
+          <p className="w-[60px] text-xl">체력</p>
+          <HpBar percentage={hp} color={'emerald'} />
+        </div>
+        <div className={`${layoutDirection === 'row' ? 'flex' : 'flex-col'}`}>
+          <p className="w-[60px] text-xl">공격력</p>{' '}
+          <HpBar percentage={attack} color={'cyan'} />
+        </div>
+        <div className={`${layoutDirection === 'row' ? 'flex' : 'flex-col'}`}>
+          <p className="w-[60px] text-xl">방어력</p>{' '}
+          <HpBar percentage={defense} color={'sky'} />
+        </div>
+        <div className={`${layoutDirection === 'row' ? 'flex' : 'flex-col'}`}>
+          <p className="w-[60px] text-xl">스피드</p>{' '}
+          <HpBar percentage={speed} color={'indigo'} />
+        </div>
+      </div>
     </div>
   );
 };
